04c1aa99669324120b10e0ccc31f927c40400a5a
Oda Opsanger <odasop@uia.no>
2023-10-17 13:25:42 +0200

    new delete function. error message.

--- journalComponents/Journal_delete.js
+++ journalComponents/Journal_delete.js
@@ -1,23 +1,53 @@
-import React, { useState } from 'react';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
-import { getFirestore, doc, deleteDoc } from 'firebase/firestore';
 import Journal_print from "../journalComponents/Journal_print";
+import Journal_comp from "../journalComponents/Journal_comp";
+import getJournalList from "../journalComponents/Journal_print";
+import {useRoute} from "@react-navigation/native";
+import {
+    Button,
+    Pressable,
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    TouchableOpacity,
+    FlatList, ActivityIndicator, ScrollView
+} from "react-native";
 
-const db = getFirestore();
+import React, { useState, useEffect } from "react";
+import { AntDesign, MaterialIcons } from '@expo/vector-icons';
+import { db } from "../firebaseConfig"
+import {
+    doc,
+    updateDoc,
+    deleteDoc,
+    getDocs,
+    query,
+    collection,
+    addDoc,
+    getDoc,
+    where,
+    setDoc,
+    getFirestore
+} from "firebase/firestore"
 
-const deleteJournal = async (journalId, userId) => {
-    /*try {
-        const journalRef = doc(db, "users", userId, "Journal", journalId);
 
-        // Delete the entire journal (document)
-        await deleteDoc(journalRef);
-        console.log(`Journal ${journalId} deleted successfully.`);
-        Journal_print();
-        // After deletion, you might want to update your journal list or perform any other necessary actions.
-    } catch (error) {
-        console.error('Error deleting journal:', error);
-    }*/
-};
+const deleteJournal = async () => {
+
+    const route = useRoute();
+    const {userId} = route.params;
+
+    const querySnapshot = await getDocs(collection(db, "users", userId, "Journal"));
+    for (const docSnap of querySnapshot.docs) {
+        const querySnapshot2 = await getDocs(collection(db, "users", userId, "Journal", docSnap.id, "entry"));
+        for (const docSnap2 of querySnapshot2.docs) {
+            await deleteDoc(doc(db, "users", userId, "Journal", docSnap.id, "entry", docSnap2.id));
+        }
+        await deleteDoc(doc(db, "users", userId, "Journal", docSnap.id));
+    }
+    Journal_print('Journal');
+
+}
 
 export default deleteJournal

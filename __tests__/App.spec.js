dd5c18c9288118fe379920f591d2f8975f9deaf8
Oda Opsanger <odasop@uia.no>
2023-11-15 16:34:54 +0100

    Added relevant files and dependencies. Setup. Tried writing very simple test. Still problematic.

---
+++ __tests__/App.spec.js
@@ -0,0 +1,8 @@
+import 'react-native';
+import React from 'react';
+import renderer from 'react-test-renderer';
+import App from '../App';
+it('App should render correctly', () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree).toMatchSnapshot();
+});

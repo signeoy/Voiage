425e482e5b5ce771f0aa2b2e998639bbbaa4ff9a
Oda Opsanger <odasop@uia.no>
2023-11-21 18:19:57 +0100

    Removed tempmenu. Removed privacy policy screen. Privacy policy screen is now functioning for the settings screen.

--- App.js
+++ App.js
@@ -28,68 +28,13 @@ import Journal_entry_page from "./journalEntryComponent/Journal_entry_page";
 
 
 import BottomTab from "./tabComponents/BottomTab";
+import ExternalLink from "./userComponents/PrivacyPolicy";
 
 
 const Stack = createStackNavigator();
 
-function TempMenu({ navigation }) {
+const privacy = 'https://voiage-oso-soy.blogspot.com/2023/11/privacy-policy.html';
 
-  return (
-      <SafeAreaView>
-        <ScrollView contentContainerStyle={{...styles.scroll_container} }>
-          <View style = {styles.header}>
-            <Text style = {{...styles.heading, textAlign:"center"}}>This is a temparary menu</Text>
-          </View>
-          <View style={{...styles.container}}>
-
-            <Pressable onPress={() => navigation.navigate('My Favourites')}>
-              <View style={{ ...styles.nav_button, backgroundColor: "#FCF6BE" }}>
-                <Text style={styles.nav_button_text}>My Favourites</Text>
-              </View>
-            </Pressable>
-
-            <Pressable onPress={() => navigation.navigate('Home')}>
-              <View style={{ ...styles.nav_button, backgroundColor: "#FCBEBE" }}>
-                <Text style={styles.nav_button_text}>Home</Text>
-              </View>
-            </Pressable>
-
-            <Pressable onPress={() => navigation.navigate('My Profile')}>
-              <View style={{ ...styles.nav_button, backgroundColor: "#FCBEBE" }}>
-                <Text style={styles.nav_button_text}>My Profile</Text>
-              </View>
-            </Pressable>
-
-            <Pressable
-                onPress={() => navigation.navigate('Privacy Policy',)}>
-              <View style={{...styles.nav_button, backgroundColor: "#F8DAC4",}}>
-                <Text style={styles.nav_button_text}>Privacy Policy</Text>
-              </View>
-            </Pressable>
-            <Pressable
-                onPress={() => navigation.navigate('Settings')}>
-              <View style={{...styles.nav_button, backgroundColor: "#CAFFCC",}}>
-                <Text style={styles.nav_button_text}>Settings</Text>
-              </View>
-            </Pressable>
-            <Pressable
-                onPress={() => navigation.navigate('Register')}>
-              <View style={{...styles.nav_button, backgroundColor: "#CAFFCC",}}>
-                <Text style={styles.nav_button_text}>Register</Text>
-              </View>
-            </Pressable>
-            <Pressable
-                onPress={() => navigation.navigate('Login')}>
-              <View style={{...styles.nav_button, backgroundColor: "#CAFFCC",}}>
-                <Text style={styles.nav_button_text}>Login</Text>
-              </View>
-            </Pressable>
-
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-  );
-}
 function HomeScreen({ navigation }) {
 
 
@@ -177,11 +122,10 @@ function SettingsScreen({ navigation }) {
             <Image
                 style={{...styles.logo}}
                 source={require('./assets/Headerlogo_text.png')} />
-            <Pressable onPress={() => navigation.navigate('Privacy Policy')}>
                 <View style={{ ...styles.nav_button, backgroundColor: "#FCF6BE" }}>
-                    <Text style={styles.nav_button_text}>Privacy policy</Text>
+                    <ExternalLink url={privacy}/>
                 </View>
-            </Pressable>
+
             <Logout navigation={navigation}/>
 
             <BottomTab navigation={navigation} />
@@ -201,14 +145,6 @@ function RegisterScreen({ navigation }) {
   return (
       <View>
           <Register navigation={navigation} setUser={setUser}/>
-      </View>
-  );
-}
-function PrivacyScreen({ navigation }) {
-
-  return (
-      <View style={{...styles.container, backgroundColor: "#CAFFCC"}}>
-            <Text>This is the privacy policy. You can read all the terms and conditions on this link:</Text>
       </View>
   );
 }
@@ -251,10 +187,6 @@ function MyStack() {
       <Stack.Navigator /*screenOptions={{headerShown: false}}*/>
         <Stack.Screen name="Login" component={LoginScreen} options={{ headerLeft: null, headerShown: false }}/>
         <Stack.Screen name="Register" component={RegisterScreen} options={{ headerLeft: null, headerShown: false }}/>
-        <Stack.Screen name="Privacy Policy" component={PrivacyScreen} />
-
-        <Stack.Screen name="TempMenu" component={TempMenu} options={{ headerLeft: null, headerShown: false }} />
-
         <Stack.Screen name="Home" component={HomeScreen}/>
         <Stack.Screen name="My Favourites" component={FavouriteScreen} options={{ headerLeft: null, headerShown: false }}/>
         <Stack.Screen name="Profile" component={ProfileScreen} />
